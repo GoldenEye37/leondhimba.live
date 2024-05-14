c252e32bc7d52800924cb71862933a3c469d522f
Leon Dhimba <dhimbaleon356@gmail.com>
2024-05-14 10:58:46 +0200

    ft:hero right content

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,19 @@ export default {
     extend: {
         fontFamily: {
             robotslab:['Roboto Slab', 'serif'],
+            poppins:['Poppins', 'sans-serif'],
         },
         width: {
+            '268': '268px',
             '303': '303px',
             '443': '443px',
+            '705': '705px',
+            '786': '786px',
+        },
+        height: {
+            '660': '650px',
+            '80': '80px',
+            '834': '834px',
         },
         backgroundColor: {
             button: {
