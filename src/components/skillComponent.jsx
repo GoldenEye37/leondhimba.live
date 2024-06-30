753a4f86f307a2ee611b36398e0fa9714c448e83
Leon Dhimba <dhimbaleon356@gmail.com>
2024-06-30 21:34:59 +0200

    feat:create skill component

---
+++ src/components/skillComponent.jsx
@@ -0,0 +1,8 @@
+export default function SkillComponent({ src, text }){
+    return (
+        <div className={`flex flex-col items-center justify-center bg-green-400 py-3 w-110`}>
+            <img src={src} alt="Skills image" className={`mb-3 w-12 h-12`}/>
+            <h2 className={`font-robotslab font-bold text-lg`}>{text}</h2>
+        </div>
+    )
+}
