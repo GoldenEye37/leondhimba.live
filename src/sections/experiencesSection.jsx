b95c0313a9829a6d20d651b2959580603753206e
Leon Dhimba <dhimbaleon356@gmail.com>
2024-07-06 22:21:49 +0200

    feat:add second experience

--- src/sections/experiencesSection.jsx
+++ src/sections/experiencesSection.jsx
@@ -7,15 +7,28 @@ export default function ExperiencesSection(){
         companyName: 'BancABC ZW',
         period: 'Aug 2022 - Aug 2023',
         responsibilities: [
-          'I developed backend REST APIs for internal and customer based applications using Spring Boot.',
-          'I facilitated deployment on test servers, and conducted bug fixing.',
-          'I tested and documented APIs using Postman and SoapUI facilitating better developer experience.',
-          'I was responsible for integrating the Bank\'s systems directly with external companies using Spring Boot.',
-          'Spearheaded automation projects using Python from scratch.',
-          'Contributed and related well with other developers on a number of projects.',
-          'Led requirements gathering, crafted Business Requirements Documents, and Business Cases.',
+            'I developed backend REST APIs for internal and customer based applications using Spring Boot.',
+            'I facilitated deployment on test servers, and conducted bug fixing.',
+            'I tested and documented APIs using Postman and SoapUI facilitating better developer experience.',
+            'I was responsible for integrating the Bank\'s systems directly with external companies using Spring Boot.',
+            'Spearheaded automation projects using Python from scratch.',
+            'Contributed and related well with other developers on a number of projects.',
+            'Led requirements gathering, crafted Business Requirements Documents, and Business Cases.',
         ],
      };
+     const experience2 = {
+        jobTitle: 'Freelance Python Backend Dev',
+        companyName: 'ModestNerds Co',
+        period: 'Nov 2022 - present',
+        responsibilities: [
+            'Collaborated the development of backend APIs for the therapy application using Python and Django REST framework',
+            'Used Django ORM to construct optimized querysets',
+            'Worked closely with my mentor in developing high-end REST APIs',
+            'Performed integrations with Stripe and Dyte',
+            'Developed backend tasks using Redis and Celery',
+        ],
+     };
+
 
     return (
         <div className={`w-full items-center mb-8`}>
@@ -32,6 +45,12 @@ export default function ExperiencesSection(){
                         companyName={experience1.companyName}
                         period={experience1.period}
                         responsibilities={experience1.responsibilities}
+                    />
+                    <ExperienceComponent
+                        jobTitle={experience2.jobTitle}
+                        companyName={experience2.companyName}
+                        period={experience2.period}
+                        responsibilities={experience2.responsibilities}
                     />
                 </div>
             </div>
