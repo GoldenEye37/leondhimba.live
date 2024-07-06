25eb48315e739d876639f5e9e2b3b7c06a86b189
Leon Dhimba <dhimbaleon356@gmail.com>
2024-07-06 22:09:24 +0200

    fix:create experience component passing prop elements

--- src/components/experienceComponent.jsx
+++ src/components/experienceComponent.jsx
@@ -1,53 +1,36 @@
 import companyImg from '../assets/company.svg';
 import calendarImg from '../assets/calendar.svg';
+import PropTypes from "prop-types";
 
 
-export default function ExperienceComponent(){
+export default function ExperienceComponent({ jobTitle, companyName, period, responsibilities }){
     return(
-        <div className={`flex flex-col items-center justify-center mx-auto pt-6 gap-y-6`}>
+        <div className={`flex flex-col items-center justify-center mx-auto pt-6 gap-y-6 min-w-full`}>
             <div className={`flex flex-row items-center bg-palette-7 h-11 px-3 max-w-sm rounded-lg`}>
-                <p className={`font-robotslab font-bold text-2xl text-center`}>Digital Transformation Intern</p>
+                <p className={`font-robotslab font-bold text-2xl text-center`}>{jobTitle}</p>
             </div>
             <div className={`font-robotslab font-normal text-lg `}>
                 <div className={`flex flex-row gap-x-3`}>
                     <img src={companyImg}/>
-                    <p>Company: BancABC ZW</p>
+                    <p>Company: {companyName}</p>
                 </div>
                 <div className={`flex flex-row gap-x-3`}>
                     <img src={calendarImg}/>
-                    <p>Period: Aug 2022- Aug 2023</p>
+                    <p>Period: {period}</p>
                 </div>
             </div>
-            <ul className="list-disc list-inside mt-4 space-y-2">
-                <li>
-                    <strong>Developed reliable backend APIs for A360 version 2.0</strong> utilizing Spring Boot REST
-                    development, resulting in a 90% improvement in API efficiency compared to the previous version.
-                </li>
-                <li>
-                    <strong>Facilitated deployment on test servers, and conducted bug fixing</strong>, ensuring smooth and reliable
-                    releases.
-                </li>
-                <li>
-                    <strong>Documented APIs using Postman and SoapUI</strong>, improving clarity and ease of use for developers and
-                    stakeholders.
-                </li>
-                <li>
-                    <strong>Integrated the bank's system directly with external companies</strong>, eliminating the need for third-party
-                    gateways and saving significant costs.
-                </li>
-                <li>
-                    <strong>Spearheaded an automation project from scratch</strong>, particularly developing the Stationery Ordering app,
-                    enhancing operational efficiency.
-                </li>
-                <li>
-                    <strong>Engineered a customer welcome solution</strong> that sends SMS notifications and emails to clients upon
-                    account creation, expediting communication.
-                </li>
-                <li>
-                    <strong>Led requirements gathering, crafted Business Requirements Documents, and Business Cases</strong>,
-                    demonstrating strong leadership, communication, and interpersonal skills.
-                </li>
+            <ul className="flex flex-col text-left list-disc list-inside mt-4 space-y-2">
+                {responsibilities.map(
+                    (responsibility, index) => <li key={index}>{responsibility}</li>
+                )}
             </ul>
         </div>
     )
 }
+
+ExperienceComponent.propTypes = {
+  jobTitle: PropTypes.string.isRequired,
+  companyName: PropTypes.string.isRequired,
+  period: PropTypes.string.isRequired,
+  responsibilities: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
--- src/sections/experiencesSection.jsx
+++ src/sections/experiencesSection.jsx
@@ -2,6 +2,21 @@ import rocketImg from '../assets/experiences 48x48.svg'
 import ExperienceComponent from "../components/experienceComponent.jsx";
 
 export default function ExperiencesSection(){
+     const experience1 = {
+        jobTitle: 'Digital Transformation Intern',
+        companyName: 'BancABC ZW',
+        period: 'Aug 2022 - Aug 2023',
+        responsibilities: [
+          'I developed backend REST APIs for internal and customer based applications using Spring Boot.',
+          'I facilitated deployment on test servers, and conducted bug fixing.',
+          'I tested and documented APIs using Postman and SoapUI facilitating better developer experience.',
+          'I was responsible for integrating the Bank\'s systems directly with external companies using Spring Boot.',
+          'Spearheaded automation projects using Python from scratch.',
+          'Contributed and related well with other developers on a number of projects.',
+          'Led requirements gathering, crafted Business Requirements Documents, and Business Cases.',
+        ],
+     };
+
     return (
         <div className={`w-full items-center mb-8`}>
             <div className={`w-auto h-auto space-y-3 items-center align-middle`}>
@@ -12,7 +27,12 @@ export default function ExperiencesSection(){
                 </div>
                 {/* section content*/}
                 <div className={`w-full px-64 font-robotslab flex flex-col items-center gap-y-8`}>
-                        <ExperienceComponent/>
+                    <ExperienceComponent
+                        jobTitle={experience1.jobTitle}
+                        companyName={experience1.companyName}
+                        period={experience1.period}
+                        responsibilities={experience1.responsibilities}
+                    />
                 </div>
             </div>
         </div>)
